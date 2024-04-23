d111d58ee88f25118f09cac55cc41b985adeaa1b
Jakub_Opielka <jakub.opielka@q-perior.com>
2024-04-23 15:23:39 +0200

    ke se ra se ra

---
+++ src/main/java/pl/polsl/universityfilesender/UniversityFileSenderApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.universityfilesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityFileSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityFileSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/polsl/universityfilesender/UniversityFileSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.universityfilesender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityFileSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
